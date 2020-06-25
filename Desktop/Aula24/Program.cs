c763e95f789a2bae34374fb2be600efe5f926a6b
unknown <villagepllayer@gmail.comgit co>
2020-06-25 15:19:18 -0300

    projeto finalizado

---
+++ Desktop/Aula24/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recursividade rec = new Recursividade();
+            rec.GerarSequenciaFibonacci(0,1,20);
+            
+            Console.WriteLine( rec.GerarFatorial(8));
+
+        }
+    }
+}
