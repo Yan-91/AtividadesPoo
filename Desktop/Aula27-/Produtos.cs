fdbf1085791e0142ecc19e38981c59a07c461133
unknown <villagepllayer@gmail.comgit co>
2020-06-30 15:31:24 -0300

    finalizado

--- Desktop/Aula27-/Produtos.cs
+++ Desktop/Aula27-/Produtos.cs
@@ -1,35 +1,91 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 
-namespace Aula27
+namespace Aula27_28_29_30
 {
     public class Produto
     {
         public int Codigo { get; set; }
         public string Nome { get; set; }
         public float Preco { get; set; }
+
+        private const string PATH = "Database/Produto.csv";
+
+        /// <summary>
+        /// Encontrar as linhas(fazer leitura)
+        /// </summary>
+        /// <returns> produtos </returns>
+        public List<Produto> Ler(){
+            // Criamos uma lista de produtos
+            List<Produto> produtos = new List<Produto>();
+
+            // Transformamos as linhas encontradas em um array de strings
+            string[] linhas = File.ReadAllLines(PATH);
+
+            //Varremos esse array de strings
+            foreach(var linha in linhas){
+
+                //quebramos cada linha em partes
+                string[] dados = linha.Split(";");
+
+                //Tratamos os dados e adicionamos um novo produto
+                Produto prod    = new Produto();
+                prod.Codigo     = Int32.Parse( Separar(dados[0]) );
+                prod.Nome       = Separar( dados[1]);
+                prod.Preco      = float.Parse( Separar(dados[2]));
+
+                // Agora adicionamos o produto tratado na lista de produtos antes de retorna-la
+                produtos.Add(prod);
+
+            }
+            return produtos;
+        }
+            //Buscar por Nome através da expressão lambda
+
+        /// <summary>
+        /// Separar conteudo apresentado para o usuário
+        /// </summary>
+        /// <param name="_coluna">Objeto</param>
+        /// <returns>Em colunas</returns>
+        private string Separar(string _coluna){
+
+            //0
+            //nome = Riven
+            return _coluna.Split("=")[1];
+        }
         
-
-        private const string PATH = "Database/produtos.csv";
-
         public Produto(){
+
             string pasta = PATH.Split('/')[0];
             if(!Directory.Exists(pasta)){
                 Directory.CreateDirectory(pasta);
             }
-            if(!File.Exists(PATH))
-            {
-              File.Create(PATH).Close();
+
+            //Criar arquivo caso n exista
+            if(!File.Exists(PATH)){
+                File.Create(PATH).Close();
             }
         }
-        public void Cadastrar(Produto prod)
-        {
-            var Linha = new string[]{ PrepararLinha(prod) };
-            
 
-            File.AppendAllLines(PATH , Linha);
+        /// <summary>
+        /// Inserir as linahs prontas no arquivo
+        /// </summary>
+        /// <param name="p">Produtos cadastrados</param>
+        public void Inserir(Produto p){
+            var linha = new string[]{ p.PeprararLinhaCSV(p) };
+            File.AppendAllLines(PATH, linha);
         }
-        public string PrepararLinha(Produto p){
-            return $"codigo={p.Codigo};nome={p.Nome};preco={p.Preco}";
+
+        /// <summary>
+        ///  Fazer as linhas
+        /// </summary>
+        /// <param name="produto">Objetos escrtios</param>
+        /// <returns>Escrever produto, nome e preço</returns>
+        private string PeprararLinhaCSV(Produto produto){
+            return $"codigo={produto.Codigo};nome={produto.Nome};preco={produto.Preco}";
         }
+
+        
     }
 }
--- Desktop/Aula27-/Program.cs
+++ Desktop/Aula27-/Program.cs
@@ -1,17 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Aula27
+namespace Aula27_28_29_30
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Produto p1 = new Produto();
-            p1.Codigo = 1;
-            p1.Nome = "Fender";
-            p1.Preco = 4500f;
+            Produto p = new Produto();
+            p.Codigo = 5;
+            p.Nome = "Lee";
+            p.Preco = 32f;
 
-            p1.Cadastrar(p1);
+            p.Inserir(p);
+
+            List<Produto> lista = p.Ler();
+
+            foreach(Produto item in lista){
+                Console.ForegroundColor = ConsoleColor.Green;
+                System.Console.WriteLine($"R$:{item.Preco} - {item.Nome}");
+            }
+
+            //Buscar por Nome através da expressão lambda
+            Produto riven = lista.Find(x => x.Nome == "Yasuo");
+
+            System.Console.WriteLine(riven.Nome);
+
+            
+
+            
         }
+        
+        
     }
 }
